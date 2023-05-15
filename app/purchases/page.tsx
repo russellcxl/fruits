8f406eeaa81c35440de8c0f9aaae24b1d367c13c
Russell Chan <russellcxl@gmail.com>
2023-05-15 21:45:17 +0800

    will format purchases tomorrow

--- app/purchases/page.tsx
+++ app/purchases/page.tsx
@@ -4,14 +4,14 @@ import { useEffect, useState } from 'react';
 
 export default function Home() {
     const [data, setData] = useState<{
-        purchase_date: Date,
+        purchase_date: string,
         purchase_value: number,
         purchased_items: string,
     }[]>([]);
     useEffect(() => {
         const fetchResponse = async () => {
             const res = await fetch('/api/get-purchases');
-            const data = await res.json();
+            let data = await res.json();
             setData(data);
         };
         fetchResponse();
@@ -20,7 +20,7 @@ export default function Home() {
         <main>
             <h1>PURCHASES</h1>
             {data.map((x) => (
-                <li>{x.purchased_items}, {x.purchase_value}</li>
+                <li>{x.purchase_date}, {x.purchased_items}, ${x.purchase_value}</li>
             ))}
         </main>
     )
