96d5874f8e4c4fe4a3a1d5f3fdbbd503e773c4aa
Russell Chan <russellcxl@gmail.com>
2023-05-15 13:49:27 +0800

    able to add items to cart

--- app/fruits/page.tsx
+++ app/fruits/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function MyForm() {
+export default function Example() {
   const [fruitName, setFruitName] = useState('');
   const [fruitPrice, setFruitPrice] = useState('');
   const [fruitImage, setFruitImage] = useState('');
@@ -19,25 +19,67 @@ export default function MyForm() {
       console.error('Error adding fruit!');
     }
   };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Fruit Name:
-        <input type="text" value={fruitName} onChange={(e) => setFruitName(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Fruit Price:
-        <input type="text" value={fruitPrice} onChange={(e) => setFruitPrice(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Fruit Price:
-        <input type="text" value={fruitImage} onChange={(e) => setFruitImage(e.target.value)} />
-      </label>
-      <br />
-      <button type="submit">Add Fruit</button>
-    </form>
-  );
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Add your fruit
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" action="#" method="POST" onClick={handleSubmit}>
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+              Name
+            </label>
+            <div className="mt-2">
+              <input
+                onChange={(e) => setFruitName(e.target.value)}
+                type="text"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+              Price
+            </label>
+            <div className="mt-2">
+              <input
+                onChange={(e) => setFruitPrice(e.target.value)}
+                type="text"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+              Image URL
+            </label>
+            <div className="mt-2">
+              <input
+                onChange={(e) => setFruitImage(e.target.value)}
+                type="text"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [data, setData] = useState<{ id: number, name: string, price: number, image_url: string }[]>([]);
+  const [cart, setCart] = useState<{ id: number, name: string, price: number, quantity: number }[]>([]);
+
   useEffect(() => {
     const fetchResponse = async () => {
       const res = await fetch('/api/get-fruits');
@@ -12,11 +14,24 @@ export default function Home() {
     };
     fetchResponse();
   }, []);
-  return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Fruits</h2>
 
+  const handleAddToCart = (fruit: { id: number, name: string, price: number }) => {
+    const existingCartItem = cart.find(item => item.id === fruit.id);
+    if (existingCartItem) {
+      setCart(prevCart => prevCart.map(item => item.id === fruit.id ? { ...item, quantity: item.quantity + 1 } : item));
+    } else {
+      setCart(prevCart => [...prevCart, { id: fruit.id, name: fruit.name, price: fruit.price, quantity: 1 }]);
+    }
+  };
+
+  const handleRemoveFromCart = (fruitId: number) => {
+    setCart(prevCart => prevCart.filter(item => item.id !== fruitId));
+  };
+
+  return (
+    <div className="bg-white mx-auto grid grid-cols-12 gap-4 p-1">
+      <div className="col-span-12 rounded-lg bg-white p-32 sm:col-span-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Fruits</h2>
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {data.map((x) => (
             <div key={x.id} className="group relative">
@@ -32,12 +47,26 @@ export default function Home() {
                     {x.name}
                   </h3>
                 </div>
+                <div className="flex items-center">
+                  <button onClick={() => handleAddToCart({ id: x.id, name: x.name, price: x.price })} className="bg-gray-800 text-white px-4 py-2 rounded-md">
+                    Add to Cart
+                  </button>
+                </div>
                 <p className="text-sm font-medium text-gray-900">${x.price}</p>
               </div>
             </div>
           ))}
         </div>
       </div>
+      <div className="col-span-12 rounded-lg border border-gray-400 bg-gray-200 p-16 sm:col-span-4">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Cart</h2>
+        <div className="mt-6">
+          {cart.map((item) => (
+            <li className='text-black'>{item.name} x {item.quantity} = ${(item.quantity * item.price).toFixed(2)}</li>
+          ))}
+        </div>
+      </div>
     </div>
+
   )
 }
