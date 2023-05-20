34adcdb44593bc10f1a00b08e3bec11c56dff786
Russell Chan <russellcxl@gmail.com>
2023-05-20 14:20:49 +0800

    order purchases desc

--- pages/api/get-purchases.ts
+++ pages/api/get-purchases.ts
@@ -6,7 +6,10 @@ const batchLimit = 500
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
         const data = await prisma.purchase.findMany({
-            take: batchLimit
+            take: batchLimit,
+            orderBy: {
+                purchase_date: 'desc'
+            },
         })
         res.status(200).json(data);
     } catch (e) {
